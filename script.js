7390d98ef69ee527aa6a645723f9b88daebcc6d1
Jackson Watt-Bowers <wattbowers@gmail.com>
2023-02-15 15:44:23 -0500

    added pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementsByTagName('form');
+const input = document.getElementsByTagName('input');
+
+form.addEventListener('submit', addTodo);
+
+const addTodo = () => {
+    
+}
